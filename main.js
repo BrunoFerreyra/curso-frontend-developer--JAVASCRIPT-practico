e53b247c98b55e2e4a98b8a2e8a76ad4c68414c9
unknown <bruno>
2023-01-29 03:28:38 -0300

    Añadi productos e imagenes de Megara desde JS

--- main.js
+++ main.js
@@ -58,4 +58,111 @@ function mostrarAside(){
     aside.classList.toggle('inactive');    
     
     
+}
+
+// Clase 4: productos
+// creamos un array de objetos, donde cada objeto que le agregamos es un producto
+
+// creo una querySelector para  el elemento cards-containar y agregarle cada productCard 
+const cardsContainer = document.querySelector('.cards-container')
+
+const productList = [];
+productList.push({
+    // cada objeto tiene los nombre precio e imagen
+    name: 'bike',
+    price: 1000,
+    image: "./iara/iara2.jpg"
+});
+productList.push({
+    name: 'megara2',
+    price: 1000,
+    image: "./iara/iara1.jpg"
+});
+productList.push({
+    name: 'Megara3',
+    price: 1500,
+    image: "./iara/iara3.jpg"
+});
+/*
+<div class="product-card">
+      <img src="./iara/iara2.jpg" alt="">
+      <div class="product-info">
+        <div>
+          <p>$120,00</p>
+          <p>Bike</p>
+        </div>
+        <figure>
+          <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+      </div>
+    </div>
+    */
+
+// Aqui creamos el html de cada producto con la estructura de arriba
+
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div')
+        productCard.classList.add("product-card");
+    
+        // product = { name, price, image}
+        // entonces tenemos que colocar product.image 
+        // es decir la imagen correspondiente de cada objeto
+        const productImg = document.createElement('img')
+        productImg.setAttribute('src',product.image);
+        
+        // hacemos lo mismo para el segundo div de clase "product-info"
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        //hacemos lo mismo para el 3 div con el precio y el nombre
+    
+        const productInfoDiv = document.createElement('div');       //este es el dive que tiene el precio y el nombre
+        
+        const productPrice = document.createElement('p')
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = document.Name;
+    
+        //inserto productPrice y productName en produictInfoDiv
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+    
+        const productInfoFigure = document.createElement('Figure');
+        const iconCart = document.createElement('img')
+        iconCart.setAttribute('src',"./icons/bt_add_to_cart.svg");
+        
+        // con creteElement() creamos cada elemento html y con classList.add() le acregamos su clase
+        // luego para ordenarlos usamos append child
+    
+        // insertamos el icono iconCart en el elemento figure
+        productInfoFigure.appendChild(iconCart)
+    
+        //despues inserto productInfoDiv y productInfoFigure en productInfo
+    
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+    
+        // y ahora finalmente inserto la imagen (img) y el productInfo adentro del div productCArd
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        //hasta aca tenemos productCard terminado.
+        //Ahora hay que insertar el productCard en el div cards-Container
+    
+        cardsContainer.appendChild(productCard)
+        
     }
+}
+
+renderProducts (productList)
+
+
+
+
+
+
+
+
+
